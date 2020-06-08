362abeb9d3233a6a86981a8a9e7b413eacf8ce3a
Douglas Dennys <douglasdennys@yahoo.com>
2020-06-07 21:33:00 -0300

    criando endpoints privados

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'API'], function () {
     Route::post('login', 'AuthController@login');
     Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
-    Route::resource('contacts', 'ContactController', ['except' => ['create', 'edit']]);
+    Route::middleware('auth:api')->group(function () {
+        Route::resource('contacts', 'ContactController', ['except' => ['create', 'edit']]);
+    });
 });
